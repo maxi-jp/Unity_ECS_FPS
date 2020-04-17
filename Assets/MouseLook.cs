63da57389f18b53b5c0df8f5d4fa44ed52979a51
Maxi <maximiliano.miranda@sngular.team>
2020-04-17 13:45:56 +0200

    Dependencie and initial assets package install

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    Vector2 rotation = new Vector2(0, 0);
+    public float speed = 3;
+
+    public bool lockCursor = true;
+    private bool m_cursorIsLocked = true;
+
+
+    void FixedUpdate()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        float z = Input.GetAxis("Vertical");
+        transform.eulerAngles = (Vector2)rotation * speed;
+        this.transform.Translate(0, 0, z * speed * 0.1f);
+
+        UpdateCursorLock();
+    }
+
+    public void SetCursorLock(bool value)
+    {
+        lockCursor = value;
+        if (!lockCursor)
+        {//we force unlock the cursor if the user disable the cursor locking helper
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void UpdateCursorLock()
+    {
+        //if the user set "lockCursor" we check & properly lock the cursos
+        if (lockCursor)
+            InternalLockUpdate();
+    }
+
+    private void InternalLockUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            m_cursorIsLocked = false;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_cursorIsLocked = true;
+        }
+
+        if (m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (!m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
+
